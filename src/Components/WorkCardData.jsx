b302cd29584c53a801473597d66d38a79ffb0a63
vishalsuriya <vishalsuriya2003@gmail.com>
2024-12-25 21:27:49 +0530

    updated project

--- src/Components/WorkCardData.jsx
+++ src/Components/WorkCardData.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import pro3 from "../assets/pet Adoption.png";
 import pro2 from "../assets/p3.png";
 import pro1 from "../assets/project1.jpg"
@@ -14,30 +14,6 @@ const ProjectData = [
               imgsrc :pro2,
               title : "ElectZone-Ecommerce ",
               text : "Collaboratively developed an eCommerce website for electronic itemsusing the MERN stack, tailored to the needs of college students.Focused on creating a user-friendly interface and intuitive shoppingexperience. ",
-=======
-import pro1 from "../assets/pet Adoption.png";
-import pro2 from "../assets/project2.png";
-import pro3 from "../assets/p3.png";
-const ProjectData = [
-       {
-        imgsrc:pro1,
-        title: "Pet Adoption System",
-        text : "SystemDeveloped a pet adoption system with features for admin to managepets through CRUD operations and for users to adopt pets based ontheir needs. Streamlined the adoption process, enhancing both adminand user experience.",
-        view : "https://petpalsconnect.netlify.app/",
-        source : "https://github.com/vishalsuriya/Pet-Adoption-System"
-       },
-       {
-        imgsrc :pro2,
-        title : "Sudoku Solver and Generator",
-        text : "Developed a sudoku game allowing users to play at variousdifficulty levels. Implemented a timer feature for enhancedgameplay experience, showcasing proficiency in front-end webdevelopment.",
-        view : "https://vishalsuriya.github.io/Sudoku-Solver-and-Generator/",
-        source :"https://github.com/vishalsuriya/Sudoku-Solver-and-Generator"
-       },
-       {
-              imgsrc :pro3,
-              title : "ElectZone-Ecommerce Website",
-              text : "Collaboratively developed an eCommerce Website tailored for electronic items,focusing on user-friendliness and designed to cater specifically to college students. This project was a team effort.",
->>>>>>> bc3513fd2fa7c325024f7878a0e972ed30ed2755
               view : "https://elect-zone.vercel.app/",
               source :"https://github.com/vishalsuriya/ElectZone"
              },
